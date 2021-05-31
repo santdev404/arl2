6938a82e60d21d1423b2cb337efecab4eccf0f17
Santdev404 <santdev404@gmail.com>
2021-05-30 21:04:15 -0500

    Add models

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,6 +11,10 @@ class User extends Authenticatable
 {
     use HasFactory, Notifiable;
 
+    public function posts(){
+        return $this->hasMany(Post::class);
+    }
+
     /**
      * The attributes that are mass assignable.
      *
@@ -40,4 +44,6 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    
 }
