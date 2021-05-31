3922604c63caa5e9628b1241845f9cfc992f95aa
Santdev404 <santdev404@gmail.com>
2021-05-31 12:29:18 -0500

    action store update

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,6 +7,8 @@ use Illuminate\Http\Response;
 
 use App\Models\Post;
 
+use App\Helpers\JwtAuth;
+
 
 class PostController extends Controller
 {
@@ -48,14 +50,110 @@ class PostController extends Controller
 
     public function store(Request $request){
         //Recoger datos por post
+        $json = $request->input('json', null);
+        $params = json_decode($json);
+        $params_array = json_decode($json, true);
 
-        //Conseguir usuario indentificado
+        if(!empty($params_array)){
+            //Conseguir usuario indentificado
+            $jwtAuth = new \App\Helpers\JwtAuth();
+            $token = $request->header('Authorization', null);
+            $user = $jwtAuth->checkToken($token, true);
 
-        //Validar los datos
+            //Validar los datos
+            $validate = \Validator::make($params_array, [
+                'title' => 'required',
+                'content' => 'required',
+                'category_id' => 'required',
+                'image' => 'required'
 
-        //Guardar el post
+            ]);
+
+            if($validate->fails()){
+                $data = [
+                    'code'=> 400,
+                    'status' => 'error',
+                    'message' => 'Faltan datos'
+                ];
+            }else{
+                $post = new Post();
+                $post->user_id = $user->sub;
+                $post->category_id = $params->category_id;
+                $post->title = $params->title;
+                $post->content = $params->content;
+                $post->image = $params->image;
+                $post->save();
+
+                //Guardar el post
+                $data = [
+                    'code'=> 400,
+                    'status' => 'success',
+                    'post' => $post
+                ];
+            }
+
+            
+        }else{
+            $data = [
+                'code'=> 400,
+                'status' => 'error',
+                'message' => 'Envia datos correctamente'
+            ];
+        }
+
+
 
         //Devolver la respuesta
+        return response()->json($data, $data['code']);
+    }
+
+
+    public function update(Request $request, $id){
+        //Recoger datos por post
+        $json = $request->input('json', null);
+        $params_array = json_decode($json, true);
+
+        //Datos para devolver
+        $data = [
+            'code' => 400,
+            'status' => 'error',
+            'message' => 'Datos enviados incorrectamente'
+        ];
+
+        if(!empty($params_array)){
+            //Validar los datos
+            $validate = \Validator::make($params_array, [
+                'title' => 'required',
+                'content' => 'required',
+                'category_id' => 'required'
+            ]);
+
+            if($validate->fails()){
+                $data['message'] = $validate->errors();
+                return reponse()->json($data, $data['code']);
+            }
+
+            //Eliminar lo que no se actualiza
+            unset($params_array['id']);
+            unset($params_array['user_id']);
+            unset($params_array['created_at']);
+            unset($params_array['user']);
+
+            //Actualizar el resgistro
+            //Get, first obtiene los objetos actualizados solo funciona con QueryBuilder
+            $post = Post::where('id', $id)->updateOrCreate($params_array);
+
+            //Regresar data
+            $data = [
+                'code' => 200,
+                'status' => 'success',
+                'post' => $post,
+                'change' => $params_array
+            ];
+            
+        }
+        return response()->json($data, $data['code']);
+
     }
 
 }
